c9e2a0abe03e96ce8053e025357db420f464ed57
Troy Chryssos <troychryssos@gmail.com>
2018-12-20 17:18:04 -0500

    got d3 actually displaying color bah gawd

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,11 +1,17 @@
 import React, { PureComponent } from 'react'
+import * as d3 from 'd3'
 import './styles.scss'
 
+const colorScale = d3.scaleLinear()
+	.domain([-100, 0, +100])
+	.range(['red', 'white', 'green'])
+
 export default class ColorPicker extends PureComponent { // eslint-disable-line
+
 	render() {
 		return (
 			<div className="colorpicker">
-
+				{colorScale(65)}
 			</div>
 		)
 	}
