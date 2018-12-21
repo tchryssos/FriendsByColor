3f430047b990694d6cf7c6a0080b08df7539915e
Troy Chryssos <troychryssos@gmail.com>
2018-12-20 20:44:16 -0500

    omfg hsl interpolator actually works

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -3,16 +3,14 @@ import * as d3 from 'd3'
 import './styles.scss'
 
 const colorScale = d3.scaleLinear()
-	.domain([-100, 0, +100])
-	.range(['red', 'white', 'green'])
+	.domain([0, 360])
+	.range([d3.hsl(0, 1, 0.5), d3.hsl(360, 1, 0.5)])
+	.interpolate(d3.interpolateHslLong)
 
 export default class ColorPicker extends PureComponent { // eslint-disable-line
-
 	render() {
 		return (
-			<div className="colorpicker">
-				{colorScale(65)}
-			</div>
+			<div className="colorpicker" style={{ backgroundColor: colorScale(150) }} />
 		)
 	}
 }
